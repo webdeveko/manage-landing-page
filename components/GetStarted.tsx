9249ce86b35e3a874318c00d6622ee8916781e48
Ekophe <genz.millenials@gmail.com>
2023-08-27 18:53:39 +0700

    first commit

---
+++ components/GetStarted.tsx
@@ -0,0 +1,19 @@
+
+const GetStarted = () => {
+  return (
+    <div className="bg-[#F3603C] ">
+    <div className="max-w-[1110px] w-full mx-auto flex justify-between items-center p-4 h-[220px]">
+      <div className="flex items-center justify-center max-w-[445px] font-bold">
+        <h2 className="text-white text-[40px]">Simplify how your team works today.</h2>
+      </div>
+      <div className="px-4  flex items-center">
+      <button className="bg-white rounded-full text-[#F3603C] text-[13px] px-5 py-2 font-semibold">
+          Get Started
+        </button>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default GetStarted
